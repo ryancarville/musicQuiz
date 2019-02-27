6e86290fbb354fc36c822e8cbdb481cc13306ba2
Ryan Carville <ryancarville@gmail.com>
2019-02-27 01:46:33 +0100

    working on skiping requiered form

--- main.js
+++ main.js
@@ -195,6 +195,10 @@ function checkUserAnswer(correctAnswerArtist, correctAnswerSong, correctAnswerDi
 //event lisener on the submit button for the user answer
 function userSubmitAnswer (song, correctAnswerArtist, correctAnswerSong, correctAnswerDisplay, genreNum) {
     if (userAnswerBtn.addEventListener('click', event => {
+        song.currentTime = 30;
+        checkUserAnswer(correctAnswerArtist, correctAnswerSong, correctAnswerDisplay, genreNum);
+    }));
+    else if (userAnswerBtnSkip.addEventListener('click', event => {
         event.preventDefault();
         song.currentTime = 30;
         checkUserAnswer(correctAnswerArtist, correctAnswerSong, correctAnswerDisplay, genreNum);
